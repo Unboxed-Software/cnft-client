113905d889e82f4b289929488dbb126035cf5219
John <johnzhengyuliu@gmail.com>
2023-06-14 15:45:38 -0500

    index.ts comments

--- src/index.ts
+++ src/index.ts
@@ -30,18 +30,28 @@ import {
 } from "@metaplex-foundation/mpl-bubblegum"
 
 async function main() {
+  // Establishing connection to Solana devnet cluster
   const connection = new Connection(clusterApiUrl("devnet"), "confirmed")
+
+  // Getting or creating the first wallet (Wallet_1)
   const wallet = await getOrCreateKeypair("Wallet_1")
+
+  // Getting or creating the second wallet (Wallet_2)
   const wallet2 = await getOrCreateKeypair("Wallet_2")
+
+  // Airdropping devnet Sol to first wallet if needed
   airdropSolIfNeeded(wallet.publicKey)
 
+  // Defining the maximum depth and buffer size for the Merkle tree
   const maxDepthSizePair: ValidDepthSizePair = {
     maxDepth: 3,
     maxBufferSize: 8,
   }
 
+  // Defining the canopy depth for the Merkle tree
   const canopyDepth = 0
 
+  // Creating the Merkle tree account
   const treeAddress = await createTree(
     connection,
     wallet,
@@ -49,59 +59,85 @@ async function main() {
     canopyDepth
   )
 
+  // Mint one compressed NFT to the first wallet
   const assetId1 = await mintCompressedNFT(connection, wallet, treeAddress)
+
+  // Mint another compressed NFT to the first wallet
   const assetId2 = await mintCompressedNFT(connection, wallet, treeAddress)
 
+  // Transfer the first NFT from Wallet_1 to Wallet_2
   await transferCompressedNFT(connection, assetId1, wallet, wallet2)
+
+  // Burn the second NFT from Wallet_1
   await burnCompressedNFT(connection, assetId2, wallet)
 }
 
+// Helper function to create a tree account and initialize it through the MPL Bubblegum program
 async function createTree(
-  connection: Connection,
-  payer: Keypair,
-  maxDepthSizePair: ValidDepthSizePair,
-  canopyDepth: number
+  connection: Connection, // The connection to Solana cluster
+  payer: Keypair, // The payer of the transaction
+  maxDepthSizePair: ValidDepthSizePair, // The maximum depth and buffer size of the tree
+  canopyDepth: number // The canopy depth of the tree
 ) {
+  // Generates a new random keypair to use as the address of the tree account
   const treeKeypair = Keypair.generate()
 
+  // Derives the tree authority PDA using the tree account address as a seed
+  // This is a PDA derived from the Metaplex Bubblegum program, allowing the program to make changes to the tree account
+  // This PDA is also used by the Metaplex Bubblegum program to initialize a "TreeConfig" account
+
+  //   pub struct TreeConfig {
+  //     pub tree_creator: Pubkey,
+  //     pub tree_delegate: Pubkey,
+  //     pub total_mint_capacity: u64,
+  //     pub num_minted: u64,
+  //     pub is_public: bool,
+  // }
   const [treeAuthority, _bump] = PublicKey.findProgramAddressSync(
     [treeKeypair.publicKey.toBuffer()],
     BUBBLEGUM_PROGRAM_ID
   )
 
+  // Instruction to create and allocate space for the tree account by invoking the system program directly.
+  // This instruction is created separately from the one initializing the tree account through the MPL Bubblegum program
+  // because the account size could exceed the limit that can be initialized through a Cross-Program Invocation (CPI).
   const allocTreeIx = await createAllocTreeIx(
-    connection,
-    treeKeypair.publicKey,
-    payer.publicKey,
-    maxDepthSizePair,
-    canopyDepth
+    connection, // The connection to Solana cluster
+    treeKeypair.publicKey, // The address of the tree account to create
+    payer.publicKey, // The payer of the transaction
+    maxDepthSizePair, // The maximum depth and buffer size of the tree
+    canopyDepth // The canopy depth of the tree
   )
 
+  // Instruction to initialize `TreeConfig` account and the Merkle tree account
+  // The tree account is initialized using a CPI to the SPL Account Compression program from the MPL Bubblegum program
   const createTreeIx = createCreateTreeInstruction(
     {
-      treeAuthority,
-      merkleTree: treeKeypair.publicKey,
-      payer: payer.publicKey,
-      treeCreator: payer.publicKey,
-      logWrapper: SPL_NOOP_PROGRAM_ID,
-      compressionProgram: SPL_ACCOUNT_COMPRESSION_PROGRAM_ID,
+      treeAuthority, // The tree authority PDA, used to initialize the "TreeConfig" account
+      merkleTree: treeKeypair.publicKey, // The address of the tree account to initialize, this account should already exist allocated with the correct space
+      payer: payer.publicKey, // The payer of the transaction
+      treeCreator: payer.publicKey, // Address set as "tree_creator" in the "TreeConfig" account
+      logWrapper: SPL_NOOP_PROGRAM_ID, // The log wrapper program, used to log data and performs no other function
+      compressionProgram: SPL_ACCOUNT_COMPRESSION_PROGRAM_ID, // The SPL Account Compression program, used to initialize the tree account
     },
     {
-      maxBufferSize: maxDepthSizePair.maxBufferSize,
-      maxDepth: maxDepthSizePair.maxDepth,
-      public: false,
+      maxBufferSize: maxDepthSizePair.maxBufferSize, // The maximum buffer size of the tree
+      maxDepth: maxDepthSizePair.maxDepth, // The maximum depth of the tree
+      public: false, // Whether the tree account is public or not
     },
-    BUBBLEGUM_PROGRAM_ID
+    BUBBLEGUM_PROGRAM_ID // The program ID of the MPL Bubblegum program
   )
 
   try {
+    // Creates a new transaction and adds the instructions
     const tx = new Transaction().add(allocTreeIx, createTreeIx)
-    tx.feePayer = payer.publicKey
+    tx.feePayer = payer.publicKey // Set the payer of the transaction fees
 
+    // Send and confirm the transaction
     const txSignature = await sendAndConfirmTransaction(
       connection,
       tx,
-      [treeKeypair, payer],
+      [treeKeypair, payer], // signers array, the treeKeypair is included because it is used to create the tree account
       {
         commitment: "confirmed",
         skipPreflight: true,
@@ -119,15 +155,17 @@ async function createTree(
   }
 }
 
+// Helper function to mint a compressed NFT to a leaf of the Merkle tree account
 async function mintCompressedNFT(
-  connection: Connection,
-  payer: Keypair,
-  treeAddress: PublicKey
+  connection: Connection, // The connection to Solana cluster
+  payer: Keypair, // The payer of the transaction
+  treeAddress: PublicKey // The address of the tree account
 ) {
-  // Compressed NFT Metadata
+  // Compressed NFT Metadata to mint to the tree
+  // This data is hashed and stored as a leaf of the Merkle tree
   const compressedNFTMetadata = createCompressedNFTMetadata(payer.publicKey)
 
-  // Derive the tree authority PDA ('TreeConfig' account for the tree account)
+  // Derive the tree authority PDA ('TreeConfig' account) from the tree account
   const [treeAuthority] = PublicKey.findProgramAddressSync(
     [treeAddress.toBuffer()],
     BUBBLEGUM_PROGRAM_ID
@@ -138,15 +176,15 @@ async function mintCompressedNFT(
     {
       payer: payer.publicKey, // The account that will pay for the transaction
       merkleTree: treeAddress, // The address of the tree account
-      treeAuthority, // The authority of the tree account, should be a PDA derived from the tree account address
+      treeAuthority, // The "TreeConfig" account, should be a PDA derived using the tree account address as a seed
       treeDelegate: payer.publicKey, // The delegate of the tree account, should be the same as the tree creator by default
       leafOwner: payer.publicKey, // The owner of the compressed NFT being minted to the tree
       leafDelegate: payer.publicKey, // The delegate of the compressed NFT being minted to the tree
-      compressionProgram: SPL_ACCOUNT_COMPRESSION_PROGRAM_ID,
-      logWrapper: SPL_NOOP_PROGRAM_ID,
+      compressionProgram: SPL_ACCOUNT_COMPRESSION_PROGRAM_ID, // The SPL Account Compression program
+      logWrapper: SPL_NOOP_PROGRAM_ID, // The log wrapper program, logs the metadata in CPI logs to be used by indexing services
     },
     {
-      message: Object.assign(compressedNFTMetadata),
+      message: Object.assign(compressedNFTMetadata), // The cNFT metadata
     }
   )
 
@@ -167,6 +205,7 @@ async function mintCompressedNFT(
 
     console.log(`https://explorer.solana.com/tx/${txSignature}?cluster=devnet`)
 
+    // Extract the asset ID from the transaction logs
     const assetId = await extractAssetId(connection, txSignature, treeAddress)
     return assetId
   } catch (err) {
@@ -175,34 +214,44 @@ async function mintCompressedNFT(
   }
 }
 
+// Helper function to transfer a compressed NFT
 async function transferCompressedNFT(
-  connection: Connection,
-  assetId: PublicKey,
-  sender: Keypair,
-  receiver: Keypair
+  connection: Connection, // The connection to Solana cluster
+  assetId: PublicKey, // The assetID of the compressed NFT to transfer
+  sender: Keypair, // The sender of the compressed NFT
+  receiver: Keypair // The receiver of the compressed NFT
 ) {
   try {
+    // Retrieve the asset data and the asset proof data using Helius Digital Assets Standard (DAS) API
     const [assetData, assetProofData] = await Promise.all([
       heliusApi("getAsset", { id: assetId.toBase58() }),
       heliusApi("getAssetProof", { id: assetId.toBase58() }),
     ])
 
+    // Destructure the required data from assetData and assetProofData
     const { compression, ownership } = assetData
     const { proof, root } = assetProofData
 
+    // Public keys for the tree, owner, and delegate
     const treePublicKey = new PublicKey(compression.tree)
     const ownerPublicKey = new PublicKey(ownership.owner)
     const delegatePublicKey = ownership.delegate
       ? new PublicKey(ownership.delegate)
       : ownerPublicKey
 
+    // Get the Merkle tree account
     const treeAccount = await ConcurrentMerkleTreeAccount.fromAccountAddress(
       connection,
       treePublicKey
     )
+
+    // Get the tree authority and canopy depth
     const treeAuthority = treeAccount.getAuthority()
     const canopyDepth = treeAccount.getCanopyDepth() || 0
 
+    // Convert the proof path into account metadata
+    // These will be used as the remaining accounts for the transfer instruction
+    // Note: may be empty if the tree canopy is large enough, meaning the required proof is stored on-chain as part of the tree account
     const proofPath: AccountMeta[] = proof
       .map((node: string) => ({
         pubkey: new PublicKey(node),
@@ -211,29 +260,31 @@ async function transferCompressedNFT(
       }))
       .slice(0, proof.length - canopyDepth)
 
+    // The new "owner" of the leaf is the receiver
     const newLeafOwner = receiver.publicKey
 
+    // Create the transfer instruction
     const transferIx = createTransferInstruction(
       {
-        merkleTree: treePublicKey,
-        treeAuthority,
-        leafOwner: ownerPublicKey,
-        leafDelegate: delegatePublicKey,
-        newLeafOwner,
-        logWrapper: SPL_NOOP_PROGRAM_ID,
-        compressionProgram: SPL_ACCOUNT_COMPRESSION_PROGRAM_ID,
-        anchorRemainingAccounts: proofPath,
+        merkleTree: treePublicKey, // The address of the tree account
+        treeAuthority, // The "TreeConfig" account, also the authority of the tree account
+        leafOwner: ownerPublicKey, // The current owner of the compressed NFT
+        leafDelegate: delegatePublicKey, // The current delegate of the compressed NFT
+        newLeafOwner, // The new owner of the compressed NFT
+        logWrapper: SPL_NOOP_PROGRAM_ID, // The log wrapper program, logs data to CPI logs to be used by indexing services
+        compressionProgram: SPL_ACCOUNT_COMPRESSION_PROGRAM_ID, // The SPL Account Compression program
+        anchorRemainingAccounts: proofPath, // If needed, remaining parts of the proof provided as remaining "accounts"
       },
       {
-        root: [...new PublicKey(root.trim()).toBytes()],
-        dataHash: [...new PublicKey(compression.data_hash.trim()).toBytes()],
+        root: [...new PublicKey(root.trim()).toBytes()], // The root hash of the tree
+        dataHash: [...new PublicKey(compression.data_hash.trim()).toBytes()], // The current data hash of the compressed NFT
         creatorHash: [
-          ...new PublicKey(compression.creator_hash.trim()).toBytes(),
+          ...new PublicKey(compression.creator_hash.trim()).toBytes(), // The creator hash of the compressed NFT
         ],
-        nonce: compression.leaf_id,
-        index: compression.leaf_id,
+        nonce: compression.leaf_id, // The leaf of the compressed NFT on the tree
+        index: compression.leaf_id, // The leaf of the compressed NFT on the tree
       },
-      BUBBLEGUM_PROGRAM_ID
+      BUBBLEGUM_PROGRAM_ID // The Bubblegum program
     )
 
     const tx = new Transaction().add(transferIx)
@@ -254,33 +305,41 @@ async function transferCompressedNFT(
   }
 }
 
+// Helper function to burn a compressed NFT
 async function burnCompressedNFT(
-  connection: Connection,
-  assetId: PublicKey,
-  payer: Keypair
+  connection: Connection, // The connection to Solana cluster
+  assetId: PublicKey, // The assetID of the compressed NFT to burn
+  payer: Keypair // The payer of the transaction
 ) {
   try {
+    // Retrieve the asset data and the asset proof data using Helius Digital Assets Standard (DAS) API
     const [assetData, assetProofData] = await Promise.all([
       heliusApi("getAsset", { id: assetId.toBase58() }),
       heliusApi("getAssetProof", { id: assetId.toBase58() }),
     ])
 
+    // Destructure the required data from assetData and assetProofData
     const { compression, ownership } = assetData
     const { proof, root } = assetProofData
 
+    // Public keys for the tree, owner, and delegate
     const treePublicKey = new PublicKey(compression.tree)
     const ownerPublicKey = new PublicKey(ownership.owner)
     const delegatePublicKey = ownership.delegate
       ? new PublicKey(ownership.delegate)
       : ownerPublicKey
 
+    // Get the Merkle tree account
     const treeAccount = await ConcurrentMerkleTreeAccount.fromAccountAddress(
       connection,
       treePublicKey
     )
+
+    // Get the tree authority and canopy depth
     const treeAuthority = treeAccount.getAuthority()
     const canopyDepth = treeAccount.getCanopyDepth() || 0
 
+    // Convert the proof path into account metadata
     const proofPath: AccountMeta[] = proof
       .map((node: string) => ({
         pubkey: new PublicKey(node),
@@ -289,26 +348,27 @@ async function burnCompressedNFT(
       }))
       .slice(0, proof.length - canopyDepth)
 
+    // Create the burn instruction
     const burnIx = createBurnInstruction(
       {
-        treeAuthority,
-        leafOwner: ownerPublicKey,
-        leafDelegate: delegatePublicKey,
-        merkleTree: treePublicKey,
-        logWrapper: SPL_NOOP_PROGRAM_ID,
-        compressionProgram: SPL_ACCOUNT_COMPRESSION_PROGRAM_ID,
-        anchorRemainingAccounts: proofPath,
+        treeAuthority, // The "TreeConfig" account, also the authority of the tree account
+        leafOwner: ownerPublicKey, // The current owner of the compressed NFT
+        leafDelegate: delegatePublicKey, // The current delegate of the compressed NFT
+        merkleTree: treePublicKey, // The address of the tree account
+        logWrapper: SPL_NOOP_PROGRAM_ID, // The log wrapper program, logs data to CPI logs to be used by indexing services
+        compressionProgram: SPL_ACCOUNT_COMPRESSION_PROGRAM_ID, // The SPL Account Compression program
+        anchorRemainingAccounts: proofPath, // If needed, remaining parts of the proof provided as remaining "accounts"
       },
       {
-        root: [...new PublicKey(root.trim()).toBytes()],
-        dataHash: [...new PublicKey(compression.data_hash.trim()).toBytes()],
+        root: [...new PublicKey(root.trim()).toBytes()], // The root hash of the tree
+        dataHash: [...new PublicKey(compression.data_hash.trim()).toBytes()], // The current data hash of the compressed NFT
         creatorHash: [
-          ...new PublicKey(compression.creator_hash.trim()).toBytes(),
+          ...new PublicKey(compression.creator_hash.trim()).toBytes(), // The creator hash of the compressed NFT
         ],
-        nonce: compression.leaf_id,
-        index: compression.leaf_id,
+        nonce: compression.leaf_id, // The leaf of the compressed NFT on the tree
+        index: compression.leaf_id, // The leaf of the compressed NFT on the tree
       },
-      BUBBLEGUM_PROGRAM_ID
+      BUBBLEGUM_PROGRAM_ID // The Bubblegum program
     )
 
     const tx = new Transaction().add(burnIx)
